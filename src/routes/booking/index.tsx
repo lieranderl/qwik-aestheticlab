bf9115b472736d745810b645f5645ba96575646c
lieranderl <19622412+lieranderl@users.noreply.github.com>
2025-02-03 20:17:52 +0100

    making new booking form

--- src/routes/booking/index.tsx
+++ src/routes/booking/index.tsx
@@ -1,20 +1,24 @@
-import { component$, useSignal, useTask$, $ } from "@builder.io/qwik";
-import { server$, type DocumentHead } from "@builder.io/qwik-city";
 import {
-  useEnvLoader,
+  $,
+  component$,
+  useComputed$,
+  useSignal,
+  useStore,
+  useTask$,
+} from "@builder.io/qwik";
+import { Form, routeAction$, server$, validator$ } from "@builder.io/qwik-city";
+import {
+  HiEnvelopeOutline,
+  HiUserOutline,
+  HiPhoneOutline,
+} from "@qwikest/icons/heroicons";
+import type { Technician, TimeSlot } from "~/types";
+import {
   useServicesLoader,
   useTechniciansLoader,
-} from "~/routes/layout";
-import type { Technician, TechnicianSlots } from "~/types";
-
-// Components
-import ContactForm from "~/components/booking/contact-form";
-import ServiceSelector from "~/components/booking/service-selector";
-import DateSelector from "~/components/booking/date-selector";
-import TimeSlots from "~/components/booking/time-slots";
-import AppointmentSummary from "~/components/booking/appointment-summary";
-import ConfirmationPanel from "~/components/booking/confirmation-panel";
-import StatusModal from "~/components/booking/status-modal";
+  useEnvLoader,
+} from "../layout";
+import { ConfirmationSidePanel } from "~/components/booking2/confirmation-side-panel";
 
 const WEEKDAYS = [
   "sunday",
@@ -40,7 +44,7 @@ const fetchTechnicianSlots = server$(
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const slots = await response.json();
+    const slots: TimeSlot[] = await response.json();
 
     if (
       !Array.isArray(slots) ||
@@ -61,353 +65,112 @@ const fetchTechnicianSlots = server$(
   }
 );
 
-const submitBooking = server$(
-  async (bookingData: {
-    api_base_url: string;
-    technicianId: string;
-    services: string[];
-    slotStart: string;
-    weekday: string;
-    email: string;
-    name: string;
-    phone: string;
-  }) => {
-    const response = await fetch(
-      `${bookingData.api_base_url}/calendar/technician/${bookingData.technicianId}`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          service_id: bookingData.services,
-          date: bookingData.slotStart,
-          weekday: bookingData.weekday,
-          user_email: bookingData.email,
-          name: bookingData.name,
-          phone: bookingData.phone,
-        }),
-      }
-    );
-
-    const data = await response.json();
-    if (!response.ok) {
-      throw new Error(data.message || "Booking failed");
-    }
-    return data;
-  }
-);
+export const useBookAppointment = routeAction$(async () => {
+  console.log("ACTION");
+});
 
 export default component$(() => {
   const servicesSignal = useServicesLoader();
   const techniciansSignal = useTechniciansLoader();
   const envs = useEnvLoader();
 
-  // Form state
-  const name = useSignal("");
-  const email = useSignal("");
-  const phone = useSignal("");
-  const selectedServices = useSignal<string[]>([]);
-  const totalDuration = useSignal(0);
-  const selectedDate = useSignal("");
-  const selectedSlot = useSignal<{ start: string; end: string } | null>(null);
-  const selectedTechnician = useSignal<Technician | null>(null);
-  const availableSlots = useSignal<TechnicianSlots[]>([]);
-  const isSubmitting = useSignal(false);
-  const showModal = useSignal(false);
-  const bookingError = useSignal<string | undefined>();
-  const showStatusModal = useSignal(false);
-  const bookingStatus = useSignal<"success" | "error">("success");
+  const name$ = useSignal("tttt");
+  const email$ = useSignal("tt@tt.er");
+  const phone$ = useSignal("456789765456");
 
-  // Store form state for modal
-  const formState = useSignal<{
-    name: string;
-    email: string;
-    phone: string;
-    services: string[];
-    date: string;
-    slot: { start: string; end: string } | null;
-    technician: Technician | null;
-  } | null>(null);
-
-  const getEligibleTechnicians = $(() => {
-    return techniciansSignal.value.filter(
-      (tech: Technician) =>
-        tech.active &&
-        selectedServices.value.every((serviceId) =>
-          tech.services.includes(serviceId)
-        )
-    );
+  
+  const IsValidForm$ = useComputed$(() => {
+    const nameValid = /^[a-zA-Z\s]{2,50}$/.test(name$.value);
+    const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email$.value);
+    const phoneValid = /^\+?[0-9\-\s]{10,15}$/.test(phone$.value);
+    return nameValid && emailValid && phoneValid;
   });
 
-  const getWeekday = $(async (date: string) => {
-    const dayIndex = new Date(date).getDay();
-    return WEEKDAYS[dayIndex];
-  });
+  const showConfirmationPanel = useSignal(false);
+  const action = useBookAppointment();
 
-  const fetchAvailableSlots = $(async (date: string) => {
-    if (!date || selectedServices.value.length === 0) {
-      availableSlots.value = [];
-      return;
-    }
+  //   console.log(action)
 
-    selectedDate.value = date;
-
-    const eligibleTechnicians = await getEligibleTechnicians();
-    const weekday = await getWeekday(date);
-
-    try {
-      const slotsPromises = eligibleTechnicians.map(
-        async (tech: Technician) => {
-          return await fetchTechnicianSlots(
-            envs.value.API_BASE_URL,
-            tech,
-            date,
-            weekday,
-            totalDuration.value
-          );
-        }
-      );
-
-      const results = await Promise.all(
-        slotsPromises.map((p) =>
-          p.catch((error) => {
-            console.error("Error fetching slots for technician:", error);
-            return null;
-          })
-        )
-      );
-
-      availableSlots.value = results.filter(
-        (result) =>
-          result !== null &&
-          result.slots &&
-          result.slots.length > 0 &&
-          result.slots.some((slot) => Object.keys(slot).length > 0)
-      );
-    } catch (error) {
-      console.error("Error fetching slots:", error);
-      availableSlots.value = [];
-    }
-  });
-
-  useTask$(({ track }) => {
-    track(() => selectedServices.value);
-
-    totalDuration.value = selectedServices.value.reduce((total, serviceId) => {
-      const service = servicesSignal.value.find((s) => s.id === serviceId);
-      return total + (service?.duration || 0);
-    }, 0);
-
-    availableSlots.value = [];
-
-    if (selectedDate.value && selectedServices.value.length > 0) {
-      void fetchAvailableSlots(selectedDate.value);
-    }
-  });
-
-  const handleSubmit = $(() => {
-    if (!selectedTechnician.value || !selectedSlot.value) {
-      return;
-    }
-
-    // Store current form state
-    formState.value = {
-      name: name.value,
-      email: email.value,
-      phone: phone.value,
-      services: [...selectedServices.value],
-      date: selectedDate.value,
-      slot: selectedSlot.value,
-      technician: selectedTechnician.value,
-    };
-
-    showModal.value = true;
-  });
-
-  const handleConfirmBooking = $(async () => {
-    isSubmitting.value = true;
-    bookingError.value = undefined;
-
-    if (!formState.value) return;
-
-    const weekday = await getWeekday(formState.value.date);
-
-    try {
-      await submitBooking({
-        api_base_url: envs.value.API_BASE_URL,
-        technicianId: formState.value.technician.id,
-        services: formState.value.services,
-        slotStart: formState.value.slot!.start,
-        weekday: weekday,
-        email: formState.value.email,
-        name: formState.value.name,
-        phone: formState.value.phone,
-      });
-
-      showModal.value = false;
-      bookingStatus.value = "success";
-      showStatusModal.value = true;
-    } catch (error) {
-      console.error("Booking error:", error);
-      bookingError.value = "Failed to book appointment. Please try again.";
-      showModal.value = false;
-      bookingStatus.value = "error";
-      showStatusModal.value = true;
-    } finally {
-      isSubmitting.value = false;
-    }
+  const handleBookButtonClick = $(() => {
+    showConfirmationPanel.value = IsValidForm$.value;
   });
 
   return (
-    <div class="min-h-screen bg-sage-50 pt-24">
+    <div class="min-h-screen bg-base-200 pt-24">
       <div class="container mx-auto px-4">
-        <div class="max-w-2xl mx-auto">
-          <h1 class="text-4xl md:text-5xl font-serif text-sage-800 mb-8 text-center">
-            Book Your Appointment
-          </h1>
-
-          <div class="bg-white p-8 rounded-lg shadow-md">
-            <div class="space-y-6">
-              <ContactForm
-                name={name.value}
-                email={email.value}
-                phone={phone.value}
-                onNameChange$={(value) => (name.value = value)}
-                onEmailChange$={(value) => (email.value = value)}
-                onPhoneChange$={(value) => (phone.value = value)}
-              />
-
-              <ServiceSelector
-                services={servicesSignal.value}
-                selectedServices={selectedServices.value}
-                totalDuration={totalDuration.value}
-                onToggleService$={(serviceId) => {
-                  if (selectedServices.value.includes(serviceId)) {
-                    const newServices = [...selectedServices.value];
-                    selectedServices.value = newServices.filter(
-                      (id) => id !== serviceId
-                    );
-                  } else {
-                    selectedServices.value = [
-                      ...selectedServices.value,
-                      serviceId,
-                    ];
-                  }
-                }}
-              />
-
-              {selectedServices.value.length > 0 && (
-                <DateSelector
-                  selectedDate={selectedDate.value}
-                  onDateChange$={(date) => {
-                    if (date !== selectedDate.value) {
-                      selectedSlot.value = null;
-                      selectedTechnician.value = null;
-                      void fetchAvailableSlots(date);
-                    }
-                  }}
-                />
-              )}
-
-              {selectedServices.value.length > 0 &&
-                availableSlots.value.length > 0 && (
-                  <TimeSlots
-                    availableSlots={availableSlots.value}
-                    selectedSlot={selectedSlot.value}
-                    selectedTechnician={selectedTechnician.value}
-                    onSlotSelect$={(slot, technician) => {
-                      if (slot.status === "busy") return;
-
-                      if (
-                        selectedSlot.value?.start === slot.start &&
-                        selectedTechnician.value?.id === technician.id
-                      ) {
-                        selectedSlot.value = null;
-                        selectedTechnician.value = null;
-                        return;
-                      }
-
-                      selectedSlot.value = {
-                        start: slot.start,
-                        end: slot.end,
-                      };
-                      selectedTechnician.value = technician;
-                    }}
+        <h1 class="text-4xl md:text-5xl mb-8 text-center font-inter font-normal">
+          Book Your Appointment
+        </h1>
+        <div class="card max-w-2xl mx-auto bg-base-100 shadow-sm">
+          <div class="card-body">
+            <Form
+              class="space-y-6 flex flex-col justify-center"
+              action={action}
+            >
+              <div>
+                <label class="input validator w-full">
+                  <HiUserOutline class="text-primary w-4 h-4" />
+                  <input
+                    name="name"
+                    type="text"
+                    placeholder="Enter you name..."
+                    pattern="^[a-zA-Z\s]{2,50}$"
+                    required
+                    bind:value={name$}
+                    disabled={showConfirmationPanel.value}
                   />
-                )}
+                </label>
+                <div class="validator-hint hidden">Please enter your name</div>
+              </div>
+              <div>
+                <label class="input validator w-full">
+                  <HiEnvelopeOutline class="text-primary w-4 h-4" />
+                  <input
+                    name="email"
+                    type="email"
+                    placeholder="mail@site.com"
+                    required
+                    bind:value={email$}
+                    disabled={showConfirmationPanel.value}
+                  />
+                </label>
+                <div class="validator-hint hidden">
+                  Please enter valid email address
+                </div>
+              </div>
+              <div>
+                <label class="input validator w-full">
+                  <HiPhoneOutline class="text-primary w-4 h-4" />
+                  <input
+                    name="phone"
+                    type="tel"
+                    placeholder="Enter your phone number..."
+                    required
+                    pattern="^\+?[0-9\-\s]{10,15}$"
+                    bind:value={phone$}
+                    disabled={showConfirmationPanel.value}
+                  />
+                </label>
+                <div class="validator-hint hidden">
+                  Please enter valid phone number
+                </div>
+              </div>
 
-              {selectedSlot.value && selectedTechnician.value && (
-                <AppointmentSummary
-                  selectedDate={selectedDate.value}
-                  selectedSlot={selectedSlot.value}
-                  selectedTechnician={selectedTechnician.value}
-                  selectedServices={selectedServices.value}
-                  services={servicesSignal.value}
-                />
-              )}
-
+              {/* <button type="submit" class="btn ">Book Appointment</button> */}
               <button
                 type="button"
-                disabled={
-                  !selectedSlot.value ||
-                  !selectedTechnician.value ||
-                  isSubmitting.value
-                }
-                class={`w-full py-3 rounded-full transition-colors ${
-                  selectedSlot.value &&
-                  selectedTechnician.value &&
-                  !isSubmitting.value
-                    ? "bg-[#8b9687] text-cream hover:bg-[#8b9687]/90"
-                    : "bg-gray-300 text-gray-500 cursor-not-allowed"
-                }`}
-                onClick$={handleSubmit}
+                class="btn"
+                onClick$={handleBookButtonClick}
+                disabled={!IsValidForm$.value}
               >
-                {isSubmitting.value ? "Booking..." : "Book Appointment"}
+                Book Appointment
               </button>
-            </div>
+
+              <ConfirmationSidePanel isOpen={showConfirmationPanel} />
+            </Form>
           </div>
-
-          {showModal.value && formState.value && (
-            <ConfirmationPanel
-              isOpen={showModal.value}
-              onClose$={$(() => {
-                showModal.value = false;
-                bookingError.value = undefined;
-              })}
-              onConfirm$={handleConfirmBooking}
-              isSubmitting={isSubmitting.value}
-              selectedDate={formState.value.date}
-              selectedSlot={formState.value.slot!}
-              selectedTechnician={formState.value.technician!}
-              selectedServices={formState.value.services}
-              services={servicesSignal.value}
-              error={bookingError.value}
-            />
-          )}
-
-          <StatusModal
-            isOpen={showStatusModal.value}
-            status={bookingStatus.value}
-            message={bookingError.value}
-            onClose$={$(() => {
-              showStatusModal.value = false;
-              if (bookingStatus.value === "success") {
-                location.assign("/");
-              }
-            })}
-          />
         </div>
       </div>
     </div>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Book Appointment - Aesthetic Lab",
-  meta: [
-    {
-      name: "description",
-      content:
-        "Book your appointment at Aesthetic Lab for beauty and wellness services",
-    },
-  ],
-};
