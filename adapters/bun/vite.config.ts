a992c3156062941dcc358d18c98123684e0a2e8b
lieranderl <19622412+lieranderl@users.noreply.github.com>
2025-02-08 18:26:41 +0100

    update

--- adapters/bun/vite.config.ts
+++ adapters/bun/vite.config.ts
@@ -19,7 +19,7 @@ export default extendConfig(baseConfig, () => {
 			bunServerAdapter({
 				ssg: {
 					include: ["/*"],
-					origin: "https://yoursite.dev",
+					origin: "https://aestheticlab.be",
 					maxWorkers: 1, // Limit Workers to 1, otherwise SSG will hang when compiling Qwik City app with `bun run --bun build`.
 				},
 			}),
